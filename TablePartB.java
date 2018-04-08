3828ac8752208a7b6d7a4b59e510366042b78723
jeffpa3791 <jeffpa3791@aol.com>
2018-04-08 18:03:38 -0400

    Add files via upload

---
+++ TablePartB.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HTableDescriptor;
+
+import org.apache.hadoop.hbase.TableName;
+
+import org.apache.hadoop.hbase.client.HBaseAdmin;
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.ResultScanner;
+import org.apache.hadoop.hbase.client.Scan;
+
+import org.apache.hadoop.hbase.util.Bytes;
+
+public class TablePartB{
+
+   public static void main(String[] args) throws IOException {
+
+    HBaseConfiguration hconfig = new HBaseConfiguration(new Configuration());
+    HBaseAdmin hbase_admin = new HBaseAdmin( hconfig );
+
+    hbase_admin.listTables();  
+    
+   }
+}
+
